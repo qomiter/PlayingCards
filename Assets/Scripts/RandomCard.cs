3fc3f7cfc91ac4315eb37f48d72b62009e52c495
qomiter <qomiter@gmail.com>
2022-01-13 21:23:05 -0500

    Initial commit

---
+++ Assets/Scripts/RandomCard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCard : MonoBehaviour
+{
+    [SerializeField] Sprite[] suit;
+    [SerializeField] GameObject card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("PickCard", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PickCard()
+    {
+        Instantiate(card, transform.position, Quaternion.identity);
+        card.GetComponent<SpriteRenderer>().sprite = suit[Random.Range(0, suit.Length + 1)];
+    }
+}
