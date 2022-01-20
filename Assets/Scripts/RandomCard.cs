848e62c8f616c3b0aa56ec32b2c3c4d8f1af10d4
qomiter <qomiter@gmail.com>
2022-01-20 16:16:55 -0500

    tweaks

--- Assets/Scripts/RandomCard.cs
+++ Assets/Scripts/RandomCard.cs
@@ -27,7 +27,7 @@ public class RandomCard : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if(cards.Count > 0)
+            if(cards.Count > 5)
             {
                 PickCard();
                 Debug.Log("Card Drawn");
@@ -46,10 +46,26 @@ public class RandomCard : MonoBehaviour
 
     void PickCard()
     {
-        GameObject destroy = Instantiate(cards[0], transform.position, Quaternion.identity);
+        GameObject destroy = Instantiate(cards[0], transform.position, Quaternion.identity) ;
+        GameObject destroy1 = Instantiate(cards[1], transform.position + new Vector3(2,0,0) , Quaternion.identity);
+        GameObject destroy2 = Instantiate(cards[2], transform.position + new Vector3(4, 0, 0), Quaternion.identity);
+        GameObject destroy3 = Instantiate(cards[3], transform.position + new Vector3(6, 0, 0), Quaternion.identity);
+        GameObject destroy4 = Instantiate(cards[4], transform.position + new Vector3(8, 0, 0), Quaternion.identity);
         var selected = cards[0];
+        var selected1 = cards[1];
+        var selected2 = cards[2];
+        var selected3 = cards[3];
+        var selected4 = cards[4];
         cards.Remove(selected);
+        cards.Remove(selected1);
+        cards.Remove(selected2);
+        cards.Remove(selected3);
+        cards.Remove(selected4);
         discards.Insert(discards.Count, selected);
+        discards.Insert(discards.Count, selected1);
+        discards.Insert(discards.Count, selected2);
+        discards.Insert(discards.Count, selected3);
+        discards.Insert(discards.Count, selected4);
         //Destroy(destroy, 1f);
     }
 
