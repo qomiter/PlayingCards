d2004b940fca163cea2002dd4a3f2443aa67d451
qomiter <qomiter@gmail.com>
2022-01-20 14:59:21 -0500

    Tweaks

--- Assets/Scripts/RandomCard.cs
+++ Assets/Scripts/RandomCard.cs
@@ -1,28 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class RandomCard : MonoBehaviour
 {
-    [SerializeField] Sprite[] suit;
-    [SerializeField] GameObject card;
+    //[SerializeField] Sprite[] suit;
+    //[SerializeField] GameObject card;
+    [SerializeField] List<GameObject> cards = new List<GameObject>();
+    [SerializeField] List<GameObject> shuffledList = new List<GameObject>();
+    [SerializeField] List<GameObject> discards = new List<GameObject>();
+    [SerializeField] List<GameObject> emptyList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("PickCard", 1f, 1f);
+        //InvokeRepeating("PickCard", 1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Shuffle();
+            Debug.Log("Cards Shuffled");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if(cards.Count > 0)
+            {
+                PickCard();
+                Debug.Log("Card Drawn");
+            }
+            else 
+            {
+                Debug.Log("Out of Cards");
+            }
+            
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            discardToDraw();
+        }
     }
 
     void PickCard()
     {
-        GameObject destroy = Instantiate(card, transform.position, Quaternion.identity);
-        card.GetComponent<SpriteRenderer>().sprite = suit[Random.Range(0, suit.Length)];
-        Destroy(destroy, 5f);
+        GameObject destroy = Instantiate(cards[0], transform.position, Quaternion.identity);
+        var selected = cards[0];
+        cards.Remove(selected);
+        discards.Insert(discards.Count, selected);
+        //card.GetComponent<SpriteRenderer>().sprite = suit[Random.Range(0, suit.Length)];
+        Destroy(destroy, 1f);
+    }
+
+    void Shuffle()
+    {
+        shuffledList = cards.OrderBy(x => Random.value).ToList();
+        cards.Clear();
+        cards.AddRange(shuffledList);
+        shuffledList.Clear();
+    }
+    
+    void discardToDraw()
+    {
+        cards.AddRange(discards);
+        discards.Clear();
     }
 }
